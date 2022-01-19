d28e9e372cca164c007b6087e340ff84a7bfeca5
mazurbeam <mazurbeam@gmail.com>
2022-01-19 14:36:58 -0800

    error test

--- scripts/error_test.ts
+++ scripts/error_test.ts
@@ -1,67 +1,27 @@
 /* External imports */
 import { ethers } from 'hardhat'
 import {
-  BigNumber,
   Wallet
 } from 'ethers'
 import 'dotenv/config'
 
 /* Internal imports */
-import { VennityCollectionFactory } from '../types'
-import { MaticToken } from '../types'
 
-let mumbaiProvider = new ethers.providers.JsonRpcProvider(
+let provider = new ethers.providers.JsonRpcProvider(
   process.env.INFURA_URL
 )
 
 let privateKey1: string = process.env.PRIVATE_KEY as string
-let l1Wallet1: Wallet = new ethers.Wallet(privateKey1, mumbaiProvider)
-let matic: MaticToken
+let l1Wallet1: Wallet = new ethers.Wallet(privateKey1, provider)
+// let matic: MaticToken
 
 
 async function main() {
-  matic = await ethers.getContractAt(
-    'IERC20',
-    '0x0000000000000000000000000000000000001010'
-  ) as MaticToken
 
-  console.log('matic token', matic)
+  console.log('wallet', l1Wallet1)
 
-  console.log('wallet address', l1Wallet1)
-  // const Factory__VennityCollectionFactory = await ethers.getContractFactory(
-  //   'VennityCollectionFactory'
-  // )
-  //
-  // adminMaticBalanceBefore = await matic.balanceOf(adminAddress)
-  //
-  // CollectionFactory = await Factory__VennityCollectionFactory
-  //   .connect(l1Wallet1)
-  //   .deploy() as VennityCollectionFactory
-  //
-  // let awaitDeployedVennityCollectionFactory = await CollectionFactory
-  //   .deployTransaction.wait()
-  //
-  // console.log(
-  //   'VennityCollectionFactory contract address: ',
-  //   CollectionFactory.address
-  // )
-  //
-  // console.log(
-  //   'Gas used to deploy VennityCollectionFactory contract: ',
-  //   awaitDeployedVennityCollectionFactory.gasUsed.toString(),
-  //   ' gas'
-  // )
-  //
-  // adminMaticBalanceAfter = await matic.balanceOf(adminAddress)
-  //
-  // // Log MATIC balance spent to deploy contract
-  // console.log(
-  //   'Amount of MATIC tokens spent by admin to deploy VennityCollectionFactory contract: ',
-  //   adminMaticBalanceBefore
-  //     .sub(adminMaticBalanceAfter)
-  //     .mul(MATIC_PRICE)
-  //     .toString()
-  // )
+  const balance = await l1Wallet1.getBalance()
+  console.log('balance', balance)
 
 }
 
