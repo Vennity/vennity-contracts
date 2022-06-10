c617a7e3caa668298b5f1829f13fd6d1005cdf83
mazurbeam <mazurbeam@gmail.com>
2022-06-10 06:08:18 -0700

    change cURI test

--- test/vennitycreator.js
+++ test/vennitycreator.js
@@ -2,16 +2,27 @@ const truffleAssert = require('truffle-assertions');
 const VennityCreator = artifacts.require("./VennityCreator.sol");
 const VenProxy = artifacts.require("VenProxy")
 
+let vennityCreatorInstance = null
+let venProxyInstance = null
+let proxyImpl = null
 contract("VennityCreator", accounts => {
   it("...should deploy proxy with VennityCreator address for implementation", async () => {
     const cURI = 'http://gateway.pinata.cloud/ipfs/QmPVyfBmV1XbP7cXRkYt65hC4FPoeLvjGfUSk9SoytBD9C'
 
-    const vennityCreatorInstance = await VennityCreator.deployed()
-    const venProxyInstance = await VenProxy.new(vennityCreatorInstance.address, cURI)
+    vennityCreatorInstance = await VennityCreator.deployed()
+    venProxyInstance = await VenProxy.new(vennityCreatorInstance.address, cURI)
 
     assert.equal(vennityCreatorInstance.address, await venProxyInstance.implementation())
 
-    const proxyImpl = await VennityCreator.at(venProxyInstance.address)
+    proxyImpl = await VennityCreator.at(venProxyInstance.address)
+    assert.equal(await proxyImpl.contractURI(), cURI)
+
+  })
+
+  it("...should change contract URI", async () => {
+    const cURI = 'http://gateway.pinata.cloud/ipfs/different'
+
+    await proxyImpl.setContractURI(cURI)
     assert.equal(await proxyImpl.contractURI(), cURI)
 
   })
