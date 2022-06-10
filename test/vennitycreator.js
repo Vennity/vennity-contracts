105e33de35adde2f743d436add0d7c30747b491d
mazurbeam <mazurbeam@gmail.com>
2022-06-10 06:03:27 -0700

    added cURI assert

--- test/vennitycreator.js
+++ test/vennitycreator.js
@@ -4,10 +4,16 @@ const VenProxy = artifacts.require("VenProxy")
 
 contract("VennityCreator", accounts => {
   it("...should deploy proxy with VennityCreator address for implementation", async () => {
+    const cURI = 'http://gateway.pinata.cloud/ipfs/QmPVyfBmV1XbP7cXRkYt65hC4FPoeLvjGfUSk9SoytBD9C'
+
     const vennityCreatorInstance = await VennityCreator.deployed()
-    const venProxyInstance = await VenProxy.new(vennityCreatorInstance.address)
+    const venProxyInstance = await VenProxy.new(vennityCreatorInstance.address, cURI)
 
     assert.equal(vennityCreatorInstance.address, await venProxyInstance.implementation())
+
+    const proxyImpl = await VennityCreator.at(venProxyInstance.address)
+    assert.equal(await proxyImpl.contractURI(), cURI)
+
   })
 
 })
