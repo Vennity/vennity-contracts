8b0ac65d0ddf432cfd18bf1d18e3ae648312500d
mazurbeam <mazurbeam@gmail.com>
2022-06-10 12:22:39 -0700

    updated test

--- test/vennitycreator.js
+++ test/vennitycreator.js
@@ -5,16 +5,27 @@ const VenProxy = artifacts.require("VenProxy")
 let vennityCreatorInstance = null
 let venProxyInstance = null
 let proxyImpl = null
+
 contract("VennityCreator", accounts => {
+  const adminAccount = accounts[0]
+  const userAccount = accounts[1]
+
   it("...should deploy proxy with VennityCreator address for implementation", async () => {
+
+    console.log('adminAccount', adminAccount)
+    console.log('userAccount', userAccount)
+
     const cURI = 'http://gateway.pinata.cloud/ipfs/QmPVyfBmV1XbP7cXRkYt65hC4FPoeLvjGfUSk9SoytBD9C'
 
-    vennityCreatorInstance = await VennityCreator.deployed()
-    venProxyInstance = await VenProxy.new(vennityCreatorInstance.address, cURI)
-
+    vennityCreatorInstance = await VennityCreator.new()
+    console.log('vennityCreatorInstance owner', await vennityCreatorInstance.owner())
+    venProxyInstance = await VenProxy.new(vennityCreatorInstance.address, cURI, userAccount, {from: adminAccount})
     assert.equal(vennityCreatorInstance.address, await venProxyInstance.implementation())
 
     proxyImpl = await VennityCreator.at(venProxyInstance.address)
+    assert.equal(userAccount, await proxyImpl.owner())
+    assert.equal(true, await proxyImpl.isAdmin(adminAccount))
+
     assert.equal(await proxyImpl.contractURI(), cURI)
 
   })
